4eceb2c30cd5147ec41ac8d729b3eb1a772d71d8
OmarLighthouse <oflores@lighthousegroup.dev>
2025-03-15 19:30:22 -0600

    Create MongoDbContext.cs

---
+++ src/WebAPI.API/WebAPI.Infrastructure/MongoDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace WebAPI.Infrastructure.Data
+{
+    public class MongoDbContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoDbContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration["MongoDB:ConnectionString"]);
+            _database = client.GetDatabase(configuration["MongoDB:DatabaseName"]);
+        }
+
+        public IMongoCollection<T> GetCollection<T>(string name)
+        {
+            return _database.GetCollection<T>(name);
+        }
+
+        public IMongoDatabase Database => _database;
+
+    }
+}
