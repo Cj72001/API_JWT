db6d5b2a09c32ef11d71597c834f6a2252716f1e
OmarLighthouse <oflores@lighthousegroup.dev>
2025-03-20 00:00:40 -0600

    Create IUserRepository.cs

---
+++ src/WebAPI.API/WebAPI.Core/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using WebAPI.Core.Entities;
+
+namespace WebAPI.Core.Interfaces.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User> GetUserById(string id);
+        Task<User> GetUserByEmail(string email);
+        Task<IEnumerable<User>> GetAllUsers();
+        Task<User> GetUserByUsername(string username);
+    }
+}
