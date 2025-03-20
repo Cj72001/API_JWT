1724badf47cb2d18432e28309324ab8e44e1a59d
OmarLighthouse <oflores@lighthousegroup.dev>
2025-03-20 00:00:25 -0600

    Create ResponseMH.cs

---
+++ src/WebAPI.API/WebAPI.Core/Entities/ResponseMH.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPI.Core.Entities
+{
+    [BsonIgnoreExtraElements]
+    public class ResponseMH
+    {
+        public int Version { get; set; }
+        public string Ambiente { get; set; }
+        public int VersionApp { get; set; }
+        public string Estado { get; set; }
+        public string CodigoGeneracion { get; set; }
+        public string SelloRecibido { get; set; }
+        public string FhProcesamiento { get; set; }
+        public string ClasificaMsg { get; set; }
+        public string CodigoMsg { get; set; }
+        public string DescripcionMsg { get; set; }
+        public List<object> Observaciones { get; set; }
+    }
+}
