d0080164bb685bbdfef7f4b6e908148c9e9c9832
OmarLighthouse <oflores@lighthousegroup.dev>
2025-03-20 00:00:08 -0600

    Create Direccion.cs

---
+++ src/WebAPI.API/WebAPI.Core/Entities/Direccion.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace WebAPI.Core.Entities
+{
+    [BsonIgnoreExtraElements]
+    public class Direccion
+    {
+        [BsonElement("departamento")]
+        public string Departamento { get; set; }
+
+        [BsonElement("municipio")]
+        public string Municipio { get; set; }
+
+        [BsonElement("complemento")]
+        public string Complemento { get; set; }
+    }
+}
