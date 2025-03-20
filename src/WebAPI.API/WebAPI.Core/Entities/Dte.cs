d97c47a8b33f3deb2d5f86e9554c0710298188ce
OmarLighthouse <oflores@lighthousegroup.dev>
2025-03-20 00:00:12 -0600

    Create Dte.cs

---
+++ src/WebAPI.API/WebAPI.Core/Entities/Dte.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPI.Core.Entities
+{
+    [BsonIgnoreExtraElements]
+    public class Dte
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("dte")]
+        public DteData DteData { get; set; }
+
+        [BsonElement("responseMH")]
+        public ResponseMH ResponseMH { get; set; }
+    }
+}
