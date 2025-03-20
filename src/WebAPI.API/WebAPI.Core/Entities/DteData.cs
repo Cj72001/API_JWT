aa753461ec518d4a99f9933caecd0b4443f56429
OmarLighthouse <oflores@lighthousegroup.dev>
2025-03-20 00:00:14 -0600

    Create DteData.cs

---
+++ src/WebAPI.API/WebAPI.Core/Entities/DteData.cs
@@ -0,0 +1,37 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPI.Core.Entities
+{
+    [BsonIgnoreExtraElements]
+    public class DteData
+    {
+        [BsonElement("identificacion")]
+        public Identificacion Identificacion { get; set; }
+
+        [BsonElement("emisor")]
+        public Emisor Emisor { get; set; }
+
+        [BsonElement("receptor")]
+        public Receptor Receptor { get; set; }
+
+        [BsonElement("cuerpoDocumento")]
+        public List<CuerpoDocumento> CuerpoDocumento { get; set; }
+
+        [BsonElement("resumen")]
+        public Resumen Resumen { get; set; }
+
+        [BsonElement("otrosDocumentos")]
+        public object OtrosDocumentos { get; set; }
+
+        [BsonElement("ventaTercero")]
+        public object VentaTercero { get; set; }
+
+        [BsonElement("apendice")]
+        public object Apendice { get; set; }
+    }
+}
