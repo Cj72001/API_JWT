a2285758e22618940aa606df921d8cca04aaf6d8
OmarLighthouse <oflores@lighthousegroup.dev>
2025-03-20 00:00:05 -0600

    Create CuerpoDocumento.cs

---
+++ src/WebAPI.API/WebAPI.Core/Entities/CuerpoDocumento.cs
@@ -0,0 +1,39 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace WebAPI.Core.Entities
+{
+    [BsonIgnoreExtraElements]
+    public class CuerpoDocumento
+    {
+        [BsonElement("numItem")]
+        public int NumItem { get; set; }
+
+        [BsonElement("cantidad")]
+        public decimal Cantidad { get; set; }
+
+        [BsonElement("codigo")]
+        public string Codigo { get; set; }
+
+        [BsonElement("uniMedida")]
+        public int UniMedida { get; set; }
+
+        [BsonElement("descripcion")]
+        public string Descripcion { get; set; }
+
+        [BsonElement("precioUni")]
+        public double PrecioUni { get; set; }
+
+        [BsonElement("montoDescu")]
+        public double MontoDescu { get; set; }
+
+        [BsonElement("ventaGravada")]
+        public double VentaGravada { get; set; }
+
+        [BsonElement("tributos")]
+        public object Tributos { get; set; }
+
+        [BsonElement("noGravado")]
+        public double NoGravado { get; set; }
+    }
+}
