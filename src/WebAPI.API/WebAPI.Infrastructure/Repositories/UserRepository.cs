ff0d28503542b47891e371f061c403f545c0b8f4
OmarLighthouse <oflores@lighthousegroup.dev>
2025-03-20 00:00:51 -0600

    Create UserRepository.cs

---
+++ src/WebAPI.API/WebAPI.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,39 @@
+﻿using MongoDB.Driver;
+using WebAPI.Core.Entities;
+using WebAPI.Core.Interfaces.Repositories;
+using WebAPI.Infrastructure.Data;
+
+namespace WebAPI.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly IMongoCollection<User> _users;
+
+        public UserRepository(MongoDbContext context)
+        {
+            _users = context.GetCollection<User>("Users");
+        }
+        public Task<User> GetUserById(string id)
+        {
+            return _users.Find(u => u.Id == id).FirstOrDefaultAsync();
+        }
+
+        public Task<IEnumerable<User>> GetAllUsers()
+        {
+            IEnumerable<User> users = _users.Find(u => true).ToList();
+            return Task.FromResult(users);
+        }
+
+        public async Task<User> GetUserByUsername(string username)
+        {
+            return await _users.Find(u => u.Username == username).FirstOrDefaultAsync();
+        }
+
+        public Task<User> GetUserByEmail(string email)
+        {
+            User user = _users.Find(u => u.Email == email).FirstOrDefault();
+
+            return Task.FromResult(user);
+        }
+    }
+}
