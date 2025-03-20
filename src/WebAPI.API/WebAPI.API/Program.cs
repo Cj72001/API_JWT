869ee502a0e2f3b49ddb06abe1a94e3ca1eee07b
OmarLighthouse <oflores@lighthousegroup.dev>
2025-03-19 23:59:14 -0600

    Update Program.cs

--- src/WebAPI.API/WebAPI.API/Program.cs
+++ src/WebAPI.API/WebAPI.API/Program.cs
@@ -1,4 +1,76 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using MongoDB.Driver;
+using System.Text;
+using WebAPI.Core.Interfaces.Repositories;
+using WebAPI.Core.Interfaces.Services;
+using WebAPI.Core.Services;
+using WebAPI.Infrastructure.Data;
+using WebAPI.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+// Configurar MongoDB
+builder.Services.AddSingleton<MongoDbContext>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IDteRepository, DteRepository>();
+builder.Services.AddScoped<IDteService, DteService>();
+
+
+// Configurar JWT (si no se encuentra en el archivo de configuracion, se buscara en las variables de entorno)
+var jwtKey = builder.Configuration["Jwt:Key"] ?? Environment.GetEnvironmentVariable("JWT_SECRET", EnvironmentVariableTarget.User);
+
+// Configurar autenticación JWT
+var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey));
+
+/*builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = securityKey,
+            *//*ValidateIssuer = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidateAudience = true,
+            ValidAudience = builder.Configuration["Jwt:Audience"],*//*
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ClockSkew = TimeSpan.Zero // Elimina el desajuste en la expiracion
+        };
+    });*/
+
+builder.Services.AddAuthentication(config =>
+{
+    config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(config =>
+{
+    config.SaveToken = true;
+    config.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = securityKey,
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("NewPolicy", app =>
+    {
+        app.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
+// Agregar autorizacion
+builder.Services.AddAuthorization();
 
 // Add services to the container.
 
@@ -16,7 +88,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseAuthorization();
+app.UseCors("NewPolicy");
+app.UseAuthentication(); // Activa la autenticacion
+app.UseAuthorization();  // Activa la autorizacion
 
 app.MapControllers();
 
