dadf753db4a3da2408bf2b4d08f31abbabec3404
OmarLighthouse <oflores@lighthousegroup.dev>
2025-03-20 00:00:58 -0600

    Create IDteService.cs

---
+++ src/WebAPI.API/WebAPI.Core/Interfaces/Services/IDteService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI.Core.DTO.Responses;
+using WebAPI.Core.Entities;
+
+namespace WebAPI.Core.Interfaces.Services
+{
+    public interface IDteService
+    {
+        Task<BaseResponse<List<Dte>>> GetAllDtesAsync();
+        Task<BaseResponse<Dte>> GetDteByCodigoGeneracionAsync(string codigoGeneracion);
+    }
+
+}
