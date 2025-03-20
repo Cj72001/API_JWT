471747b90d9ba3f2da45c3fcfa0719ce79d12984
OmarLighthouse <oflores@lighthousegroup.dev>
2025-03-20 00:00:00 -0600

    Create BaseResponse.cs

---
+++ src/WebAPI.API/WebAPI.Core/DTO/Responses/BaseResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPI.Core.DTO.Responses
+{
+    public class BaseResponse<T>
+    {
+        public int StatusCode { get; set; }
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public T? Data { get; set; }
+        public Dictionary<string, string> Errors { get; set; }
+
+        public BaseResponse()
+        {
+            Errors = new Dictionary<string, string>();
+        }
+    }
+}
