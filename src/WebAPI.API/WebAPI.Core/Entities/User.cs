d694b1a16db50983aaa6956b0b93f96d038f61a4
OmarLighthouse <oflores@lighthousegroup.dev>
2025-03-19 23:59:42 -0600

    Update User.cs

--- src/WebAPI.API/WebAPI.Core/Entities/User.cs
+++ src/WebAPI.API/WebAPI.Core/Entities/User.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace WebAPI.Core.Entities
 {
     public class User
     {
-        public int Id { get; set; }
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        [BsonElement("email")]
+        public string Email { get; set; }
+        [BsonElement("Username")]
         public string Username { get; set; }
+        [BsonElement("password")]
         public string Password { get; set; }
     }
 }
