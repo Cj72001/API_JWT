9591581aa26fe0651f678f7014e8de3bb366d916
OmarLighthouse <oflores@lighthousegroup.dev>
2025-03-20 00:00:55 -0600

    Create IAuthService.cs

---
+++ src/WebAPI.API/WebAPI.Core/Interfaces/Services/IAuthService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI.Core.DTO.Request;
+using WebAPI.Core.DTO.Responses;
+
+namespace WebAPI.Core.Interfaces.Services
+{
+    public interface IAuthService
+    {
+        Task<BaseResponse<TokenResponseDTO>> Accesstoken(TokenRequestDTO requestDto);
+        public string HashPassword(string password);
+        public bool VerifyPassword(string password, string hashedPassword);
+    }
+}
