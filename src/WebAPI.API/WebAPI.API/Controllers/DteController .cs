8030bfcc2d69fb348fd4c9659d09c4f80843f2f0
OmarLighthouse <oflores@lighthousegroup.dev>
2025-03-20 00:01:06 -0600

    Create DteController .cs

---
+++ src/WebAPI.API/WebAPI.API/Controllers/DteController .cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Core.Interfaces.Services;
+
+[Route("api/[controller]")]
+[ApiController]
+[AllowAnonymous]
+public class DteController : ControllerBase
+{
+    private readonly IDteService _dteService;
+
+    public DteController(IDteService dteService)
+    {
+        _dteService = dteService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllDtes()
+    {
+        var response = await _dteService.GetAllDtesAsync();
+        return StatusCode(response.StatusCode, response);
+    }
+
+    [HttpGet("{codigoGeneracion}")]
+    public async Task<IActionResult> GetDteByCodigoGeneracion(string codigoGeneracion)
+    {
+        var response = await _dteService.GetDteByCodigoGeneracionAsync(codigoGeneracion);
+        return StatusCode(response.StatusCode, response);
+    }
+}
