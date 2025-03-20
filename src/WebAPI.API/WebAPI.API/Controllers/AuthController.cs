ae65cc2447fef01df2b7eb354c6c425701ca989d
OmarLighthouse <oflores@lighthousegroup.dev>
2025-03-20 00:01:08 -0600

    Create AuthController.cs

---
+++ src/WebAPI.API/WebAPI.API/Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Core.DTO.Request;
+using WebAPI.Core.Interfaces.Services;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly IAuthService _authService;
+
+    public AuthController(IAuthService authService)
+    {
+        _authService = authService;
+    }
+
+    [AllowAnonymous]
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] TokenRequestDTO requestDTO)
+    {
+        var response = await _authService.Accesstoken(requestDTO);
+        return Ok(response);
+    }
+
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]  // 🔒 Solo accesible con un token valido
+    [HttpGet("secure-data")]
+    public IActionResult GetSecureData()
+    {
+        return Ok(new { message = "¡Acceso concedido! Este es un endpoint protegido." });
+    }
+
+}
+
