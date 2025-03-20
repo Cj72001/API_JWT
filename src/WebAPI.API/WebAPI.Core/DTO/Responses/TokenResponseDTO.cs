458e20bf364e1875fcc0f34b79f86145726c3094
OmarLighthouse <oflores@lighthousegroup.dev>
2025-03-20 00:00:34 -0600

    Create TokenResponseDTO.cs

---
+++ src/WebAPI.API/WebAPI.Core/DTO/Responses/TokenResponseDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPI.Core.DTO.Responses
+{
+    public class TokenResponseDTO
+    {
+        public string AccessToken { get; set; }
+    }
+}
