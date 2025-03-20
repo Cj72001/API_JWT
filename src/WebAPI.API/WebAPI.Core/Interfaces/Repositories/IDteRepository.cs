d503c292b87a9d0bf3cdcdc24d4c62dbad5d5c2a
OmarLighthouse <oflores@lighthousegroup.dev>
2025-03-20 00:00:46 -0600

    Create IDteRepository.cs

---
+++ src/WebAPI.API/WebAPI.Core/Interfaces/Repositories/IDteRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI.Core.Entities;
+
+namespace WebAPI.Core.Interfaces.Repositories
+{
+    public interface IDteRepository
+    {
+        Task<List<Dte>> GetAllAsync();
+        Task<Dte> GetByCodigoGeneracionAsync(string codigoGeneracion);
+    }
+}
